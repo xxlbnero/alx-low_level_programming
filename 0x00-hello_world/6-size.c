556cf10588f58bd06872fe8991f44cc56d380b67
Nerus <n.nwoko2023@gmail.com>
2023-02-16 04:42:01 -0800

    C program to print the size of the various types running on the computer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main entry of our program
+ * @void - No argument expected
+ *
+ * Description: Print the various types on the computer is is compiled and run
+ * Return: 0 on successful program execution
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)", sizeof(char));
+	printf("Size of an int: %d byte(s)", sizeof(int));
+	printf("Size of a long int: %d byte(s)", sizeof(long));
+	printf("Size of a long long int: %d byte(s)", sizeof(double));
+	printf("Size of a float: %d byte(s)", sizeof(float));
+
+	return (0);
+}
