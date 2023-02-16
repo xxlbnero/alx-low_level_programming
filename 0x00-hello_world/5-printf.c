6e8dda88f7645bc6a23de9862dbeccbe5c3e99b4
Nerus <n.nwoko2023@gmail.com>
2023-02-16 04:14:33 -0800

    C program to print a string using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main entry point
+ * @void - No argument expected
+ *
+ * Description: A program to to print a string using the printf function
+ * --followed by a newline
+ * Return: 0 on successful program execution
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
