94966021ef1cd8f4d326cb90a4c247a0c482aa18
Nerus <n.nwoko2023@gmail.com>
2023-02-16 03:50:19 -0800

    One-liner  C program to print a string using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a given string specified
+ * @void: No argument required
+ *
+ * Description: A simple function to print the string value
+ *
+ * Return: 0 on successful execution of program
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
