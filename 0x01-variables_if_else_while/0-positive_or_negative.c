64b565f0e83a646125515ee13c20b2471fa7fa14
Nerus <n.nwoko2023@gmail.com>
2023-02-17 11:17:30 -0800

    C program to print the sign of a number

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main program entry point
+ * @void - No argument expected
+ *
+ * Description: Print if a randomly generated number is positive or negative
+ * Return: 0 on successful execution
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive", n);
+	else if (n == 0)
+		printf("%d is 0", n);
+	else
+		printf("%d is negative", n);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,3 @@
+(0) 0-positive_or_negative.c -> Check if a randomly generated number is positve, negative or 0, and display it.
+
+
