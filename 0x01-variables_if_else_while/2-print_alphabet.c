8829d170ca03a2f769ca365e939817cc1be4df12
Nerus <n.nwoko2023@gmail.com>
2023-02-17 12:49:31 -0800

    Print the letters of the alphabet using the putchar fnx

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main entry point of program
+ * @void - No argument expected
+ *
+ * Description: Print the letters of the alphabet using the putchar function
+ *
+ * Return: 0 on successful execution of program
+ */
+int main(void)
+{
+	int i = 97;
+
+	while(i < 123)
+	{
+		putchar(i);
+		i = i + 1;
+	}
+	putchar(10);
+	return (0);
+}
