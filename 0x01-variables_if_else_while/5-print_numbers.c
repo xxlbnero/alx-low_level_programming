ab36f666440bb650a213385660a7a4eaeaab0667
Nerus <n.nwoko2023@gmail.com>
2023-02-17 13:06:32 -0800

    Print digits from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main entry point of program
+ * @void - No argument expected
+ *
+ * Description: Print digit from 0 to 9
+ * Return: 0 on successful execution of program
+ */
+
+int main(void)
+{
+	int i;
+	
+	i = 48;
+
+	for (; i < 58; i++)
+		putchar(i);
+	putchar(10);
+	return(0);
+}
