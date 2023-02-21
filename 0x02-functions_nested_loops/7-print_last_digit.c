bd7f7efc41f272dc3f113c156acf15c91e89f6cd
Nerus <n.nwoko2023@gmail.com>
2023-02-21 08:08:38 -0800

    Return the last digit of a given number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Return the last digit  of a given number
+ * @n: integer value expected as parameter
+ *
+ * Return: last digit of number
+ */
+
+int print_last_digit(int n)
+{
+	ab_n = _abs(n);
+
+	return (ab_n % 10);
+}
