817f3094eb8e2976c08f1dc7ed0e5996e9ef18f4
Nerus <n.nwoko2023@gmail.com>
2023-02-21 13:41:57 -0800

    Print the times table up to nine

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - Function to print the nine times table
+ * @void: No argument required
+ *
+ * Return: Null
+ */
+
+void times_table(void)
+{
+	int row, col, tab;
+	int lim = 9;
+
+	for (row = 0; row <= lim; row++)
+	{
+		_putchar("\n");
+
+		for (col = 0; col <= lim; col++)
+		{
+			tab = row * col;
+
+			if (col == lim)
+			{
+				_putchar('0' + tab);
+			}
+			else
+			{
+				_putchar('0' + tab + ',' + ' ' + ' ');
+			}
+		}
+	}
+}
