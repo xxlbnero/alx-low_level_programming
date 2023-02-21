5246b43586486ff81f897b8ca9ab4aa3e51a4edc
Nerus <n.nwoko2023@gmail.com>
2023-02-21 06:04:44 -0800

    Print a string using custom _putchar function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - main entry point of program
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar("_putchar\n");
 
 	return (0);
 }
