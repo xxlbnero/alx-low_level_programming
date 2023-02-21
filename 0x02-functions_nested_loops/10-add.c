f04601a133f6e8d148a7658edd08f829b442806e
Nerus <n.nwoko2023@gmail.com>
2023-02-21 14:45:16 -0800

    Add two integer values and return their result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Add two integer values
+ * @a: first integer to add
+ * @b: second integer to add
+ *
+ * Return: sum of added values of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
