075d89d07d07b5a331b5f17a93f06d59b8d7cd65
Nerus <n.nwoko2023@gmail.com>
2023-02-21 15:02:03 -0800

    Print number n up to but not exceeding 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,9 +10,11 @@
 
 void print_to_98(int n)
 {
+	if (n == 98)
+		return (n);
 	if (n < 98)
 	{
-		while (n < 98)
+		while (n <= 98)
 		{
 			printf("%d, ", n);
 			n++;
@@ -23,7 +25,7 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-		while (n > 98)
+		while (n >= 98)
 		{
 			printf("%d, ", n);
 			n--;
