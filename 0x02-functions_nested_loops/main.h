dee7b0fd4cc8eef4fd4bcf87ab7c8b726a413dd6
Nerus <n.nwoko2023@gmail.com>
2023-02-21 05:38:15 -0800

    main header files for project tasks

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN.h
+#define MAIN.h
+
+/**
+ * Header files used for ALX tasks of
+ * functions and nest loops.
+ * Mainly containing function prototypes.
+ */
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+
+
+#endif
