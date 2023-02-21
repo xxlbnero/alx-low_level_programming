2b4111e9161359acf0f61d2bb7bc41b673a4df3c
Nerus <n.nwoko2023@gmail.com>
2023-02-21 14:59:04 -0800

    Print a number up to but not exceeding 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Print all natural numbers up to 98
+ * @n: Starting point
+ *
+ * Return: Null
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+
+			if (n == 98)
+				break;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+
+			if (n == 98)
+				break;
+		}
+	}
+	_putchar('\n');
+}
