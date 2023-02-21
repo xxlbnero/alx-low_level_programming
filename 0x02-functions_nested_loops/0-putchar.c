abe6d479a4166f62d22c42cb39e91d1c978cdf29
Nerus <n.nwoko2023@gmail.com>
2023-02-20 23:36:28 -0800

    Functions, Nested Loops Tasks

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main entry point of program
+ * @void - No argument required
+ *
+ * Descritpion: A program to print a text to screen
+ * Return: 0 on successful execution of program
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+(0) 0-putchar.c -> Print a text to standard output followed by a newline
+
+
