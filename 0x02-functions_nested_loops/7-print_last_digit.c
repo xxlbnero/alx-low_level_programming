7d2ad9b06f362d496728ed69531833eeda8ad837
Nerus <n.nwoko2023@gmail.com>
2023-02-21 09:06:18 -0800

    Print and return the last digit of a given number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,12 +9,22 @@
 
 int print_last_digit(int n)
 {
-	int last_digit;
+	char l = n % 10;
+	int step = 0;
 
-	int ab_n = _abs(n);
-
-	last_digit = ab_n % 10;
-	_putchar(last_digit);
-
-	return (last_digit);
+	if (n % 10 > 0)
+	{
+		_putchar(l);
+		return (l);
+	}
+	else
+	{
+		while (l < 0)
+		{
+			step += 1;
+			++l;
+		}
+	}
+	_putchar(step);
+	return (step);
 }
