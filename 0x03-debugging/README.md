187487d564ca8a550c4069e56fa9bb4027e9ee59
Nerus <n.nwoko2023@gmail.com>
2023-02-22 01:33:58 -0800

    Initial commit to debugging

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,4 @@
+(0) 0-main.c -> Tests wether 0 is positve or negative
+(0) main.h -> main header file containing function prototypes
+
+
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Main header file containing prototypes of various
+ * methods.
+ */
+
+void positive_or_negative(int i);
+
+
+
+
+#endif	/* MAIN_H */
