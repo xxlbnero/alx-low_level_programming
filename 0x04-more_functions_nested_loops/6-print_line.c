1f2680750efc34cd882f776767aceb621f5dfead
Nerus <n.nwoko2023@gmail.com>
2023-02-23 08:27:03 -0800

    fix

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,19 +8,13 @@
 
 void print_line(int n)
 {
-	int i;
+	int i = 1;
 
-	if (n <= 0)
+	while (i < n)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (i = 0; i < n; ++i)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
+		_putchar('_');
+
+		i += 1;
 	}
 	_putchar('\n');
 }
