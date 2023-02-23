5109675fa661762b9892fcffbc2ef784486d1679
Nerus <n.nwoko2023@gmail.com>
2023-02-23 08:04:58 -0800

    fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,24 +8,18 @@
 
 void more_numbers(void)
 {
-	int j;
-	int lim;
+	int i, j;
 
-	for (lim = 0; lim < 10; lim++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if (j < 10)
-			{
-				_putchar(j + '0');
-			}
+			_putchar((j % 10) + '0');
 			if (j > 9)
 			{
 				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
 			}
 		}
 		_putchar('\n');
 	}
-
 }
