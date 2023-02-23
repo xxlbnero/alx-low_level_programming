49c01f844a89b381d544b7c465e88f79c4abecb7
Nerus <n.nwoko2023@gmail.com>
2023-02-23 11:04:46 -0800

    Print a square of # using _putchar function

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - print n number of # square
+ * @size: size of # to print
+ * Return: Null
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			_putchar(35);
+		}
+		_putchar(35);
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
