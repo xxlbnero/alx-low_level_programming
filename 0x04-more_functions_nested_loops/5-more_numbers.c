28b0222f3cae34a3fc8bef7ca33b6e592faf9640
Nerus <n.nwoko2023@gmail.com>
2023-02-23 06:50:55 -0800

    Print number 0 through 14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -17,11 +17,11 @@ void more_numbers(void)
 	{
 		for (i = 0; i < 15; i++)
 		{
-			if (i / 10)
+			if (i > 9)
 			{
-				_putchar(i / 10);
+				_putchar((i / 10) + '0');
 			}
-			_putchar(i % 10 + '0');
+			_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
 	}
