20b9a2461e3a3bcbb4f33ed9869b6cf41bdef4bc
Nerus <n.nwoko2023@gmail.com>
2023-02-23 05:38:41 -0800

    Print n number of _ using _putchar function

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,16 +10,15 @@ void print_line(int n)
 {
 	int i;
 
-	i = 0;
 	if (n <= 0)
 	{
 		_putchar('\n);
 	}
 	else
 	{
-		for (; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
-			_putchar('_' + '13');
+			_putchar(95);	
 		}
 		_putchar('\n');
 	}
