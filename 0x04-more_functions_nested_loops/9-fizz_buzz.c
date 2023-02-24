fa33f408dd2a5b61947e2f0758a49bddc0e68573
Nerus <n.nwoko2023@gmail.com>
2023-02-24 04:17:54 -0800

    Print 1 to 100 subbing multiples of 3 and 5 with specific text

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,19 +19,20 @@ void main(void)
 	{
 		if (i % 3 == 0)
 		{
-			printf("%s", *mul_three);
+			printf("%s ", *mul_three);
 		}
 		else if (i % 5 == 0)
 		{
-			printf("%s", *mul_five);
+			printf("%s ", *mul_five);
 		}
 		else if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("%s", *mul_three_five);
+			printf("%s ", *mul_three_five);
 		}
 		else
 		{
-			printf("%d", i);
+			printf("%d ", i);
 		}
 	}
+	printf('\n');
 }
