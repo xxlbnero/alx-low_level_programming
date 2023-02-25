627eb121a71b76f78ccfe107e774d2b7783f0238
Nerus <n.nwoko2023@gmail.com>
2023-02-25 03:41:31 -0800

    Print the time table up to nine using custom _putchar function

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,51 +3,37 @@
 /**
  * times_table - Function to print the nine times table
  * @void: No argument required
- *
  * Return: Null
  */
 
 void times_table(void)
 {
-	int i, j;
-	int product;
+	int i, j, product;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
 			product = (i * j);
-
 			if (product < 10)
 			{
 				_putchar(' ');
 				if (j == 9)
-				{
 					_putchar((product % 10) + '0');
-				}
 				else
-				{
 					_putchar((product % 10) + '0');
 					_putchar(',');
 					_putchar(' ');
-				}
 				continue;
 			}
 			if (product > 9)
-			{
 				_putchar((product / 10) + '0');
-			}
-
 			if (j == 9)
-			{
 				_putchar((product % 10) + '0');
-			}
 			else
-			{
 				_putchar((product % 10) + '0');
 				_putchar(',');
 				_putchar(' ');
-			}
 		}
 		_putchar(10);
 	}
