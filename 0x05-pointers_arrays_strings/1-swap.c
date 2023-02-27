31acd179a732684e354f79930b876e5cbb510695
Nerus <n.nwoko2023@gmail.com>
2023-02-27 00:57:26 -0800

    Swaps two integers using pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values of two integers using pointers
+ * @a: first integer
+ * @b: second integer
+ * Return: Null
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *tmp;
+
+	*tmp = *a;
+	*a = *b;
+	*b = *tmp;
+}
