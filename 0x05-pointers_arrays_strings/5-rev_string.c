7cb6a0b1ba63328de69dadbfc9390a73dfcd47b3
Nerus <n.nwoko2023@gmail.com>
2023-02-27 04:09:21 -0800

    Reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,11 +15,10 @@ void rev_string(char *s)
 
 	for (i = 0; s[i] != '\0'; i++, str_count++)
 		;
-	while (str_count)
+	while (str_count >= 0)
 	{
 		_putchar(s[str_count]);
 		str_count -= 1;
 	}
-	_putchar(s[0]);
 	_putchar(10);
 }
