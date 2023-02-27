2fddcfc2fe690ccb7e5859b2924b12e2e5715ba6
Nerus <n.nwoko2023@gmail.com>
2023-02-27 00:50:34 -0800

    Main header file containing function prototypes for 0x05

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void reset_to_98(int n*);
+void reset_to_98(int *n);
 int _putchar(int);
-void swap_int(int a*, int b*);
+void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
