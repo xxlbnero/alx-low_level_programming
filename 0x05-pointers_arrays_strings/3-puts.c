a89c9af0cf3e40dc539bb1d3b483565690ef2a65
Nerus <n.nwoko2023@gmail.com>
2023-02-27 01:18:34 -0800

    Prints a given string to standard output

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a given string to standard output
+ * @str: string of characters
+ * Return: Null
+ */
+
+void _puts(char *str)
+{
+	int idx;
+
+	idx = 0;
+
+	while (str[idx] != '\0')
+	{
+		_putchar(s[idx]);
+
+		idx += 1;
+	}
+	_putchar('\n');
+}
