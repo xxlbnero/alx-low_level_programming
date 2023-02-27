8976341e06c1a34760faa20372534a7268d28042
Nerus <n.nwoko2023@gmail.com>
2023-02-27 11:02:12 -0800

    Copy a string from source to destination

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy a string from soruce to destination including the terminatin
+ * character
+ * @dest: destination pointer of copy operation
+ * @src: Source pointer of copy operation
+ * Return: Pointer to the destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int index;
+
+	index = 0;
+
+	while (src[index] != '\0')
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	dest[index + 1] = '\0';
+
+	return (*dest);
+}
