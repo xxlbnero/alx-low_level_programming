dd5d052893f4a9a0fda595696d0e3a74d0574aa6
Nerus <n.nwoko2023@gmail.com>
2023-02-27 11:24:46 -0800

    fix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,6 +14,12 @@ char *_strcpy(char *dest, char *src)
 
 	index = 0;
 
+	if (src[index] == '\0')
+	{
+		dest[index] = '\0';
+		return (dest);
+	}
+
 	while (src[index] != '\0')
 	{
 		dest[index] = src[index];
