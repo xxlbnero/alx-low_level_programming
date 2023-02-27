06201a65d556195a7fc39abbc5ef7005c76666d6
Nerus <n.nwoko2023@gmail.com>
2023-02-27 01:10:48 -0800

    Return the length if a given string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - given a char string, return it's length
+ * @s: String
+ * Return: Length of s
+ */
+
+int _strlen(char *s)
+{
+	int count, i;
+
+	count = 0;
+	i = 0;
+
+	while(s[i] != '\0')
+	{
+		count += 1;
+		i++;
+	}
+	return (count);
+}
