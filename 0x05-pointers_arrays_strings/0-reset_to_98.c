b6a46cd41dc15d7ebe3b4b5c4e1374ac1dda51c4
Nerus <n.nwoko2023@gmail.com>
2023-02-27 00:47:05 -0800

    Reset a given integer to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - using custom _putchar funcion, reset a given
+ * integer to 98
+ * @n: number to reset
+ * Return: Null
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
