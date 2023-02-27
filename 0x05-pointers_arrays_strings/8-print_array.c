f6917da59f9bea1aece904abcae093e933d89dfc
Nerus <n.nwoko2023@gmail.com>
2023-02-27 13:58:55 -0800

    Print numbers in an array in the order they are inputted

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_array - Print n elements of an arry
+ * @a: array to print
+ * @n: number of element
+ * Return: Null
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+
+	for (; i < n; i++)
+	{
+		printf("%d, ", *(a + i));
+	}
+	printf("%d\n", a[n]);
+}
