9627067677e36580d87b6ef3a56c59b40cf9cf8f
Nerus <n.nwoko2023@gmail.com>
2023-02-28 05:22:30 -0800

    Reverse a string in-place

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,8 +8,7 @@
 
 void rev_string(char *s)
 {
-	char *t;
-	int last_index, target, i;
+	int last_index, i;
 
 	last_index = 0;
 
@@ -17,9 +16,9 @@ void rev_string(char *s)
 	for (i = 0; s[i] != '\0'; i++, last_index++)
 		;
 
-	target = last_index;
+	const int target = last_index;
 	/* Create an array the size of the length of sing */
-	char c[last_index];
+	char c[target];
 
 	/* Copy each element in s starting from the last */
 	for (i = 0; i < target; i++)
