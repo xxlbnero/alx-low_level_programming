e9800940d6583978bcc07ecb3d90de65d073c8e4
Nerus <n.nwoko2023@gmail.com>
2023-03-01 13:45:06 -0800

    Compares two string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compare two strings for equality
+ * @s1: first string
+ * @s2: second string
+ * Return: 0 if strings are equal, negative if s1 is less than s2,
+ * positive if s1 is greater than s2.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, j, res;
+
+	for (i = 0, j = 0; s1[i] != '\0', s2[j] != '\0'; i++, j++)
+	{
+		if (s1[i] == s2[j])
+			;
+		else
+		{
+			res = s1[i] - s2[j];
+		}
+	}
+
+	return (res);
+}
