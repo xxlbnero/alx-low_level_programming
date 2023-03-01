02f8d4b550789b78af413c81b0429f584c236ed2
Nerus <n.nwoko2023@gmail.com>
2023-03-01 15:48:41 -0800

    Compares two string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,7 +16,9 @@ int _strcmp(char *s1, char *s2)
 	for (i = 0, j = 0; s1[i] != '\0' || s2[j] != '\0'; i++, j++)
 	{
 		if (s1[i] == s2[j])
-			;
+		{
+			continue;
+		}
 		else
 		{
 			res = s1[i] - s2[j];
