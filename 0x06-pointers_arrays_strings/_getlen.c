7833c9ad0ab9c2b2ba8db4518cc5f9b75e847227
Nerus <n.nwoko2023@gmail.com>
2023-03-01 02:16:57 -0800

    Custom get length function

---
+++ 0x06-pointers_arrays_strings/_getlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/***
+ * _getLen - Given an input string, return it's length
+ * @str: Input string
+ * Return: Length of str
+ */
+
+
+int _getLen(char *str)
+{
+	int count, i;
+
+	count = 0;
+
+	for (i = 0; str[i] != '\0'; i++, count++)
+		;
+	return (count);
+}
