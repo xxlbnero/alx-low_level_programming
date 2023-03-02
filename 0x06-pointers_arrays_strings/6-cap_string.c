a69fb692a61e7a8b04915fe9a5913afe1aa3ed75
Nerus <n.nwoko2023@gmail.com>
2023-03-01 22:26:16 -0800

    Capitalize word in a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -31,11 +31,10 @@ char *cap_string(char *str)
 			flag = 0;
 		}
 		/* Now the delimeters */
-		if (str[i] == 9 || str[i] == 10 || str[i] == 32 || str[i] == 44\
-		|| str[i] == 58 || str[i] == 59 || str[i] == 33\
-		|| str[i] == 34 || str[i] == 63 || str[i] == 40\
-		|| str[i] == 41 || str[i] == 123 ||\
-		str[i] == 125 || str[i] == 46)
+		if (str[i] == 9 || str[i] == 10 || str[i] == 32 || str[i] == 44
+		|| str[i] == 58 || str[i] == 59 || str[i] == 33 || str[i] == 34
+		|| str[i] == 63 || str[i] == 40 || str[i] == 41 || str[i] == 123
+		|| str[i] == 125 || str[i] == 46)
 		{
 			/* delimiter found. Turn on the flag */
 			flag = 1;
