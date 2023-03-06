82000ac931f735d72ebe087605f770d4863d4fe8
Nerus <n.nwoko2023@gmail.com>
2023-03-06 11:40:28 -0800

    Fills memory with constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with constant byte
+ * @s: buffer to fill
+ * @b: char to fill buffer with
+ * @n: n number of bytes to fill
+ *
+ * Return: Pointer to memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; s[i] || i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
