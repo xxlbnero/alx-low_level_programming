66fd63379a20a8987e2fe946d7843d575c73db53
Nerus <n.nwoko2023@gmail.com>
2023-03-06 12:47:04 -0800

    Prints the chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_chessboard -  prints the chess board
+ * @a: a pointer to an array of elements
+ * Return: NUll
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			if (a[i][j] == 32)
+			{
+				continue;
+			}
+			_putchar(a[i][j]);
+		}
+		_putchar(10);
+	}
+	_putchar(10);
+}
