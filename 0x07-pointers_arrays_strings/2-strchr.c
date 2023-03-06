5514f9fb42f30f4c846d320770307902de48c802
Nerus <n.nwoko2023@gmail.com>
2023-03-06 12:17:17 -0800

    Copies memory area

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: character string
+ * @c: character to locate
+ * Return: Pointer to the first occurance of character c
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == c)
+		{
+			/* point s to the address of s[i] */
+			s = &s[i];
+			return (s);
+		}
+	}
+
+	return(NULL);
+}
