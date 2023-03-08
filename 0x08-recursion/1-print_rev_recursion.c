8aeb1bbc8597b56a558492f2e26b4831695a2c64
Nerus <n.nwoko2023@gmail.com>
2023-03-08 10:44:42 -0800

    print a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print a string in reverse
+ * @s: input string
+ * Return: Null
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+	}
+	_putchar(*s--);
+
+	if (!*s)
+	{
+		_putchcar(10);
+	}
+}
