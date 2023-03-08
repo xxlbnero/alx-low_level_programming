d2b77571e1cc92b53e3e09f005f3534ce4bfd562
Nerus <n.nwoko2023@gmail.com>
2023-03-08 09:15:06 -0800

    print a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,14 +8,13 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
+	if (!*s)
 	{
 		_putchar(10);
 	}
-	_putchar(10);
+	else
+	{
+		_putchar(*s);
+		_put_recursion(s + 1);
+	}
 }
