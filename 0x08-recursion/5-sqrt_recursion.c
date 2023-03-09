c110b92c0ee548872d1facb2c25301021564f818
Nerus <n.nwoko2023@gmail.com>
2023-03-09 04:00:47 -0800

    square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,54 +1,39 @@
 #include "main.h"
 
 /**
- * sqrt_aux - auxilary functon to calculate the sqrt of a number
- * @x: input of type int
- * @low: lower limit of type int
- * @high: upper limit of type int
- * Return: square root of input x
+ * sqrt_recur - find the square root of a number
+ * @x: input number of type integer
+ * @g: sub val
+ * Return: estimated value of g
  */
 
-int sqrt_aux(int x, int low, int high)
+int sqrt_recur(int x, int g)
 {
-	int mid;
-	int square;
+	int n_g = (g + x / g) / 2;
 
-	mid = (low + high) / 2;
-	square = mid * mid;
-
-	if (square == x)
+	if (n_g == g)
 	{
-		return (mid);
+		return (g);
 	}
-	else if (square < x)
-	{
-		return (sqrt_aux(x, mid, high));
-	}
-	else
-	{
-		return (sqrt_aux(x, low, mid));
-	}
+	return (sqrt_recur(x, n_g));
 }
 
-
 /**
- * _sqrt_recursion - calcluate and return the sqrt of a number
- * @n: input number
- * Return: Square root of n
+ * _sqrt - find the square root of a number
+ * @x: input number
+ * Return:  square root of x
  */
 
-int _sqrt_recursion(int n)
+int _sqrt(int x)
 {
-	if (n < 0)
+	if (x < 0)
 	{
 		return (-1);
 	}
-	else if (n == 0 || n == 1)
+	if (x == 0 || x == 1)
 	{
-		return (n);
+		return (x);
 	}
-	else
-	{
-		return (sqrt_aux(n, 0, n));
-	}
+	return (sqrt_recur(x, x / 2));
+}
 
