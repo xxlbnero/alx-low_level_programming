f15b679c593de8ef81e9c0017921ca1c7dce21f8
Nerus <n.nwoko2023@gmail.com>
2023-03-10 15:46:54 -0800

    print program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,10 +4,12 @@
  * main - print program name, followed by a newline
  * @argc: argument count
  * @argv: argument list
- * Return: Null
+ * Return: 0 on successful execution of program
  */
 
-void main(int argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
+
+	return (0);
 }
