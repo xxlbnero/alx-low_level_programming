c59852b190f323d1e5d81218a5c0b2e238247407
Nerus <n.nwoko2023@gmail.com>
2023-03-14 02:29:47 -0700

    create and return a 2D array of ints

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * alloc_grid - create a two dimentional array of ints
+ * @width: width of array
+ * @height: hieght of said array
+ * Return: pointer to a 2D array of ints
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **mptr;
+	int i, j;
+
+	if (width < 1 || height < 1)
+	{
+		return (NULL);
+	}
+	mptr = (int **)malloc(height * sizeof(int *));
+
+	if (mptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < width; i++)
+	{
+		mptr[i] = (int *)malloc(width * sizeof(int));
+	}
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < height; j++)
+		{
+			mptr[i][j] = 0;
+		}
+	}
+	return (mptr);
+}
